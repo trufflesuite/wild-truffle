639be152b580ed60dced8da2e17f8901df094eb5
cgewecke <christophergewecke@gmail.com>
2018-08-14 23:07:47 -0700

    Fix install ci script

--- scripts/install_ci.sh
+++ scripts/install_ci.sh
@@ -13,6 +13,22 @@ npm install -g yarn
 npm install -g lerna
 git clone https://github.com/trufflesuite/truffle.git
 
+# Load TRUFFLE_BRANCH variable
+source .wildtruffle
+source .wildganache
+
+echo ""
+echo "Checking out Truffle @ $TRUFFLE_BRANCH..."
+echo ""
+
+cd truffle
+git checkout $TRUFFLE_BRANCH
+npm run bootstrap
+cd packages/truffle
+npm run build
+chmod +x build/cli.bundled.js
+cd ../../..
+
 if [ "$COLONY" = true ]; then
   echo ""
   echo "Installing colonyNetwork ..."
@@ -23,19 +39,10 @@ if [ "$COLONY" = true ]; then
   cd colonyNetwork
   yarn
   git submodule update --init
-  node ../../scripts/linkTruffle.js colony
+
+  NEWPATH="./../../truffle/packages/truffle/build/cli.bundled.js"
+  sed -i.bak "s|truffle|$NEWPATH|g" package.json
+  sed -i.bak 's|"test:contracts"|"wildtruffle"|g' package.json
+  npm run wildtruffle
 fi
 
-# Load TRUFFLE_BRANCH variable
-source .wildtruffle
-#source .wildganache
-
-echo ""
-echo "Checking out Truffle @ $TRUFFLE_BRANCH..."
-echo ""
-
-# Run
-cd truffle
-git checkout $TRUFFLE_BRANCH
-npm run bootstrap
-chmod +x truffle/packages/truffle/build/cli.bundled.js
